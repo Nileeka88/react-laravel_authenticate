bfcc9971e5234f3f5fc756a2efe1d6aa423656a8
Nileeka88 <ranasinghe.nileeka@gmail.com>
2025-03-31 15:29:47 +0530

    Create user sign-in endpoint

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function () {
+Route::post('/sign-in', function () {
     //return 'hello laravel api';
     return ['message' => 'Hello laravel Api'];
 });
