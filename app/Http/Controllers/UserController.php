b51e4ef9fbfa44450db93acc1271a428f031c464
Nileeka88 <ranasinghe.nileeka@gmail.com>
2025-03-31 20:23:12 +0530

    Define routing system in frontend

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+
+class UserController extends Controller
+{
+    public function userSignIn(Request $request):JsonResponse{
+        //Log::info('i am working');
+        Log::info($request->email);
+        $user  = User::where('email',$request->email)->first();
+
+        if(!$user){
+            return response()->json(['message' => 'User not found']);
+        }
+
+
+        if($this->isValidateUserCredentials($request->all(), $user)){
+
+            return response()->json([
+                        'message' => 'User found',
+                        'user_token' => $user->createToken('testToken', ['server:admin'])->plainTextToken,
+
+                ]);
+        }
+        return response()->json(['message' => 'User credentials not valid']);
+       // Log::info($user );
+        // return ['message'=>'controller working'];
+    }
+
+    public function isValidateUserCredentials(array $request, User $user):bool{
+
+        return $user->email === $request['email'] &&  Hash::check($request['password'],$user->password);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/sign-in', function () {
-    //return 'hello laravel api';
-    return ['message' => 'Hello laravel Api'];
-});
+Route::post('/sign-in', [userController::class, 'userSignIn']);
