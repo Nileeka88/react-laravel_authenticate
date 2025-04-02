21166b6dc32853895521872fe33efbe5a1207264
Nileeka88 <ranasinghe.nileeka@gmail.com>
2025-04-02 21:32:28 +0530

    Create form functionalities with type definition

--- resources/frontend/src/components/signin/SignIn.tsx
+++ resources/frontend/src/components/signin/SignIn.tsx
@@ -1,10 +1,40 @@
-import React from 'react';
+import React, { useState } from "react";
+import SignInForm from "./SignInForm.tsx";
+import { ISignInState } from "../../utilities/types/signin/SignIn";
+import * as axios from "axios";
 
-const SignIn:React.FC = () => {
+const SignIn: React.FC = () => {
+    const [signDetails, setSignDetails] = useState<ISignInState>({
+        email: "",
+        password: "",
+    });
+
+    const handleInputField = (
+        event: React.ChangeEvent<HTMLInputElement>,
+    ): void => {
+        const { name, value } = event.target;
+        //console.log(name, value)
+        setSignDetails((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
+    };
+    //console.log(signDetails)
+
+    const handleSubmit = async (event: React.FormEvent): Promise<void> => {
+        event.preventDefault()
+        console.log(signDetails)
+        return await axios.get('/sanctum/csrf-cookie').then(res => {
+            const response = axios.post('http://127.0.0.1:8000/api/sign-in', signDetails)
+
+            console.log(response)
+        });
+    };
     return (
-        <div>
-            <h1>SIGN IN</h1>
-        </div>
+        <SignInForm
+            handleInputField={handleInputField}
+            handleSubmit={handleSubmit}
+        />
     );
 };
 
